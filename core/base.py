fb71d2b9ae8cb5262b51d4241a1b7807ef1e0d6e
GNZTMPZ <git@gnztmpz.eu.org>
2022-11-10 11:25:20 +0700

    add error handling

--- core/base.py
+++ core/base.py
@@ -18,3 +18,47 @@ class CustomClient(Client):
         self.logger.info(
             "Note: Discord needs up to an hour to load your global commands / context menus. They may not appear immediately\n"
         )
+
+    async def on_command_error(self, ctx, error):
+        """Gets triggered on a command error"""
+        if isinstance(error, CommandCheckFailure):
+            if isinstance(ctx, InteractionContext):
+                symbol = "/"
+
+            await ctx.send(
+                embeds=Embed(
+                    description="<:cross:839158779815657512> I'm afraid I can't let you use that",
+                    color=0xFF0000,
+                ),
+                ephemeral=True,
+            )
+            self.logger.warning(
+                f"Check failed on Command: [{symbol}{ctx.invoke_target}]"
+            )
+
+        elif isinstance(error, CommandOnCooldown):
+            if isinstance(ctx, InteractionContext):
+                symbol = "/"
+
+            await ctx.send(
+                embeds=Embed(
+                    description=f"<:cross:839158779815657512> Cooldown is active for this command. You'll be able to use it in {int(error.cooldown.get_cooldown_time())} seconds",
+                    color=0xFF0000,
+                ),
+                ephemeral=True,
+            )
+            self.logger.warning(
+                f"Command Ratelimited for {int(error.cooldown.get_cooldown_time())} seconds on: [{symbol}{ctx.invoke_target}]"
+            )
+        
+        elif isinstance(error, HTTPException):
+            if isinstance(ctx, InteractionContext):
+                symbol = "/"
+
+                await ctx.send(
+                    embeds=Embed(
+                        description=f"<:cross:839158779815657512> Something happened while trying to process your request, Please try again later!",
+                        color=0xFF0000,
+                    ),
+                    ephemeral=True,
+                )
